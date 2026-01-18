7316e3ee42488c193b5858fb3619e3f3d00fdd61
Aravinthan-J <aravinthanjai@gmail.com>
2026-01-18 20:31:34 +0530

    added

--- apps/mobile/android/app/src/main/java/com/habittracker/MainApplication.kt
+++ apps/mobile/android/app/src/main/java/com/habittracker/MainApplication.kt
@@ -13,8 +13,7 @@ import com.facebook.react.config.ReactFeatureFlags
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-// Flipper is not compatible with Expo
-// import com.facebook.react.flipper.ReactNativeFlipper
+import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
 
 import expo.modules.ApplicationLifecycleDispatcher
@@ -53,10 +52,9 @@ class MainApplication : Application(), ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
-    // Flipper is not compatible with Expo
-    // if (BuildConfig.DEBUG) {
-    //   ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
-    // }
+    if (BuildConfig.DEBUG) {
+      ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
+    }
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
 
